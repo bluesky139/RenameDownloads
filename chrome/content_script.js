ecb44990e0541344ecc5545e788bce360229a07d
bluesky139 <bluesky139@gmail.com>
2020-10-31 16:01:00 +0800

    Chrome is not giving full url of referrer, get from clicked element message.

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -3,30 +3,31 @@ var originalFilename;
 
 loadRules(function(rules) {
 	var rule = getRuleByUrl(window.location.href, rules);
-	if (!rule) {
-		return;
-	}
 	init(rule);
 });
 
 function init(rule) {
-	document.addEventListener("mousedown", function(event){
-		console.log("mousedown event button " + event.button);
-		clickedElement = event.target;
-		console.log(clickedElement);
-	}, true);
-
+	if (rule) {
+		document.addEventListener("mousedown", function(event){
+			console.log("mousedown event button " + event.button);
+			clickedElement = event.target;
+			console.log(clickedElement);
+		}, true);
+	}
+	
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		if(request.action == "get_filename_by_clicked_element") {
 			originalFilename = request.originalFilename;
 			console.log("Requesting get_filename_by_clicked_element, original filename " + originalFilename);
-			console.log(rule.code);
 			
 			var filename = null;
-			try {
-				filename = eval(rule.code);
-			} catch (err) {
-				console.log('Can\'t get filename, ' + err);
+			if (rule) {
+				console.log(rule.code);
+				try {
+					filename = eval(rule.code);
+				} catch (err) {
+					console.log('Can\'t get filename, ' + err);
+				}	
 			}
 			console.log('Filename: ' + filename);
 			sendResponse({ filename: filename, referrer: location.href });
